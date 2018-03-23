076d3d3576a327eca6aac076f9bd228b88e28e3d
Hallahan <sean.hallahan@accenturefederal.com>
2018-03-23 14:02:40 -0400

    factorial

--- recursion/factorial.js
+++ recursion/factorial.js
@@ -3,3 +3,14 @@ Implement factorial.
 
 factorial(5) => 5*4*3*2*1 => 120
 */
+
+function factorial(input) {
+	// base case input < 2
+
+	if (input < 2) {
+		return 1;
+	}
+	return input * factorial(input - 1);
+}
+
+factorial(10);
