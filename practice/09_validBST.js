668ec6df62285d15682301453dcc5378add00f69
Hallahan <sean.hallahan@accenturefederal.com>
2018-03-26 12:51:34 -0400

    adding practice questions

---
+++ practice/09_validBST.js
@@ -0,0 +1,21 @@
+/*
+Write a function to check that a binary tree ↴ is a valid binary search tree. ↴
+
+Here's a sample binary tree node class:
+
+  function BinaryTreeNode(value) {
+    this.value = value;
+    this.left  = null;
+    this.right = null;
+}
+
+BinaryTreeNode.prototype.insertLeft = function(value) {
+    this.left = new BinaryTreeNode(value);
+    return this.left;
+};
+
+BinaryTreeNode.prototype.insertRight = function(value) {
+    this.right = new BinaryTreeNode(value);
+    return this.right;
+};
+*/
---
+++ practice/10_secondLargestInBST.js
@@ -0,0 +1,22 @@
+/*
+
+Write a function to find the 2nd largest element in a binary search tree. ↴
+
+Here's a sample binary tree node class:
+
+  function BinaryTreeNode(value) {
+    this.value = value;
+    this.left  = null;
+    this.right = null;
+}
+
+BinaryTreeNode.prototype.insertLeft = function(value) {
+    this.left = new BinaryTreeNode(value);
+    return this.left;
+};
+
+BinaryTreeNode.prototype.insertRight = function(value) {
+    this.right = new BinaryTreeNode(value);
+    return this.right;
+};
+*/
